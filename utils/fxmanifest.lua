e59604d1e468f046c7d5ae938ddbafd70a8e4003
JustinMartinDev <justin.martin.developper@gmail.com>
2024-07-17 01:18:15 +0200

    move .git to parent directory

---
+++ utils/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'An awesome dude'
+description 'An awesome, but short, description'
+version '1.0.0'
+
+client_script 'utils.lua'
+exports { "spreadObject" }
